3c2638106b68cd3951feb9aac948ce2c2a648d3d
gustaeps2004 <gustaeps@gmail.com>
2025-09-14 02:09:18 -0300

    adicionando tela de login

---
+++ ChatBoxFront/src/components/auth/NewLogin.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="main-div">
+    <div class="image-div">
+      <img 
+        src="../../../imgs/login.svg" 
+        alt="woman sitting at the table with a computer" />
+    </div>
+    <div class="login">
+      <div class="box-login">
+        <div class="box-login-inputs">
+          <h2>LOGIN</h2>
+          <input id="login" type="text" placeholder="Login" required>
+          <input id="password" type="password" placeholder="Password" required>
+          <button>Sing in</button>
+          <label for="">New to Chat Box? 
+            <a href="">Create an account</a>
+          </label>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.main-div {
+  display: flex;
+  height: 85vh;
+  width: 80vw;
+}
+
+.image-div {
+  flex: 1; 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.image-div img {
+  max-width: 80%; 
+  max-height: 80%;
+}
+
+.login {
+  flex: 1; 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.box-login-inputs {
+  height: 40vh;
+  width: 30vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
+  padding: 20px;
+  background-color: #303030;
+  border-radius: 30px;  
+  box-shadow: 10px 10px 10px #202020;
+}
+
+.box-login-inputs input {
+  width: 70%;
+  height: 5px;
+  padding: 14px 12px;
+  font-size: 15px;
+  border: 1.5px solid #d1d5db;
+  border-radius: 8px;
+  transition: border-color .2s ease, box-shadow .2s ease;
+  background-color: #f5f5f5;
+  color: black;
+}
+
+.box-login-inputs button {
+  width: 75%;
+  height: 37px;
+  font-size: 15px;
+  margin-top: 10px;
+  border: none;
+  border-radius: 8px;
+}
+
+.box-login-inputs button:hover {
+  transition: 0.3s;
+  background-color: green;
+}
+</style>
